ae3027655ff242cf6f0f92582209c11d8c7f724a
Sravya <Sravyar202@gmail.com>
2024-08-29 19:52:09 -0400

    second commit

--- script4.sh
+++ script4.sh
@@ -10,3 +10,6 @@ echo "Hello $name, Welcome to Linux classes"
 echo "Please enter the course you're willing to learn"
 read course
 echo "You are interested in $course course"
+echo "please enter your date of Birth:"
+read date of birth
+ echo " your date of birth is $date:"
